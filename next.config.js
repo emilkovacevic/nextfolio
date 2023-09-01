c4a74df719c566b53685f9f4ae5dc17c8215d286
Emil Kovacevic <77700903+emilkovacevic@users.noreply.github.com>
2023-09-01 14:42:37 +0200

    fix analytics attempt

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const nextConfig = {
     removeConsole:
       process.env.NODE_ENV === 'production' ? { exclude: ['error'] } : false
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)', // Applies to all routes
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value:
-              "default-src *; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src *; img-src *; style-src * 'unsafe-inline';"
-          }
-        ]
-      }
-    ]
-  },
   images: {
     domains: ['images.unsplash.com', 'plus.unsplash.com']
   }
