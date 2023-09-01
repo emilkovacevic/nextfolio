7bec4a4d10477ce81734a34829f3af33d960248e
Emil Kovacevic <77700903+emilkovacevic@users.noreply.github.com>
2023-09-01 08:34:56 +0200

    changed swipper js control buttons

--- components/Swipper.tsx
+++ components/Swipper.tsx
@@ -5,29 +5,37 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/pagination'
 import 'swiper/css/navigation'
-import { Pagination, Navigation } from 'swiper/modules'
+import { Pagination, Navigation, Autoplay } from 'swiper/modules'
 import React, { ReactNode } from 'react'
 
 interface SwipperProps {
   children: ReactNode
 }
 
+const LeftArrow = () => <div className="swiper-custom-button-left">←</div>
+
+const RightArrow = () => <div className="swiper-custom-button-right">→</div>
+
 export default function Swipper({ children }: SwipperProps) {
   return (
     <Swiper
+      navigation={{
+        nextEl: '.swiper-custom-button-right',
+        prevEl: '.swiper-custom-button-left'
+      }}
+      keyboard
       pagination={{
         type: 'fraction'
       }}
-      navigation
-      autoplay={{
-        pauseOnMouseEnter: true,
-        delay: 2500,
-        disableOnInteraction: false
-      }}
       loop
-      modules={[Pagination, Navigation]}
-      className="mySwiper "
+      autoplay={{
+        delay: 4500
+      }}
+      modules={[Pagination, Navigation, Autoplay]}
+      className="mySwiper"
     >
+      <LeftArrow />
+      <RightArrow />
       {React.Children.map(children, (child: ReactNode) => (
         <SwiperSlide>{child}</SwiperSlide>
       ))}
